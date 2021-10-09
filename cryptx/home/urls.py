88e06f8fa339f4f252de55b4a5636aaa3caf7b34
ShobhitAgarwal <shobhitagarwal151@gmail.com>
2021-10-10 01:52:18 +0530

    login created

--- cryptx/home/urls.py
+++ cryptx/home/urls.py
@@ -8,5 +8,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('',views.home,name='home'),
     path('signup/',views.signup,name='signup'),
-    path('signuph/',views.signuphandle,name='signup')
+    path('signuph/',views.signuphandle,name='signup'),
+    path('login/',views.login_page,name='login_page'),
+    path('logout/',views.logout_user,name='logout')
 ]
--- cryptx/home/views.py
+++ cryptx/home/views.py
@@ -9,17 +9,24 @@ from django.contrib.auth.models import User
 from django.contrib import messages
 from uuid import uuid4
 
+
 def home(request):
     return render(request,'home/index.html')
 
+
 def signup(request):
+    user = request.user
+    if user.is_authenticated:
+        return redirect('dashboard')
     return render(request,'home/signup.html')
+
+
 
 def signuphandle(request):
 
     user = request.user
     if user.is_authenticated:
-        return redirect('home')
+        return redirect('dashboard')
 
     if request.method == 'POST':
         fname = request.POST.get('fname',"")
@@ -39,10 +46,47 @@ def signuphandle(request):
                 print("Email already taken")
                 return redirect('home')
 
-        if password==cpassword:
-            user = User.objects.create_user(fname,email,password)
-            user.first_name = fname
-            user.last_name = lname
-            user.save()
+        new_user = User.objects.create_user(fname,email,password)
+        new_user.first_name = fname
+        new_user.last_name = lname
+        new_user.save()
+
+        user = authenticate(username=email,password=password)
+        if user:
+            login(request,user)
+            return redirect('dashboard')
     
-        return render(request,'home/index.html')
+    return redirect('home')
+
+
+
+def login_page(request):
+    user=request.user
+    if user.is_authenticated:
+        return redirect('dashboard')
+    
+    if request.method=='POST':
+        email = request.POST.get('email',"")
+        password = request.POST.get('password',"")
+
+        cur_user = User.objects.filter(username=email)
+        if not cur_user:
+            print("No such user")
+            return redirect('signup')
+
+        cur_user=cur_user[0]
+        if cur_user.password!=password:
+            print("Passwords didn't matched")
+            return redirect('login_page')
+
+        user = authenticate(username=email,password=password)
+        if user:
+            login(request,user)
+            return redirect('dashboard')
+
+    return render(request,'login.html')
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('home')
